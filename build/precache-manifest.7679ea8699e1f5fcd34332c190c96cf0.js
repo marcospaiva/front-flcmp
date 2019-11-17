95d8bad0066e0e93405d8a2e980e3ca9b9f7da13
Marcos <marcos@nuvemshop.com.br>
2019-11-17 18:32:38 -0300

    Add build files

---
+++ build/precache-manifest.7679ea8699e1f5fcd34332c190c96cf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c116cc63ff7f9ffbb7239fc2298da9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83728ecec614f5dc3fae",
+    "url": "/static/css/main.b78e1e2d.chunk.css"
+  },
+  {
+    "revision": "a135a0c6c04ee62496a1",
+    "url": "/static/js/2.d91375e7.chunk.js"
+  },
+  {
+    "revision": "83728ecec614f5dc3fae",
+    "url": "/static/js/main.e41dea5c.chunk.js"
+  },
+  {
+    "revision": "bb7e7463357611dc8d83",
+    "url": "/static/js/runtime-main.fca44c8b.js"
+  }
+]);

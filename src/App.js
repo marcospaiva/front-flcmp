6399145b928b005ba7ae117652d0d0f86b5e5165
Marcos <marcos@nuvemshop.com.br>
2020-06-20 23:56:40 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React, {useState, useEffect} from 'react';
-import Header from './Header';
-import Phrase from './Phrase';
-import SharingButtons from './SharingButtons';
-import Footer from './Footer';
-import api from './Api';
+import React, {useState, useEffect} from 'react'
+import Phrase from './Phrase'
+import Header from './Header'
+import Footer from './Footer'
+import api from './Api'
 
 function App() {
-  const [phrase, setPhrase ] = useState([]);
+  const [phrase, setPhrase ] = useState([])
 
   useEffect( () => {
     async function loadPhrases(){
       const response = await api.get();
       setPhrase(response.data);
 
-      document.title = response.data;
+      document.title = response.data
     }
     loadPhrases();
   }, []);
 
   return (
-    <section className="app">
+    <>
       <Header />
-      <Phrase phrase={phrase} />
-      <SharingButtons />
+      <main>
+        <Phrase phrase={phrase} />
+      </main>
       <Footer />
-    </section>
+    </>
   );
 }
 

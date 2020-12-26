a4d4c975af45735b40e1ea8138665e8d1bc1fe58
Marcos <marcos@nuvemshop.com.br>
2020-12-26 09:37:40 -0300

    Add light font

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles/fonts/HKGroteskWide-Black.woff'
 import './styles/fonts/HKGroteskWide-Black.woff2'
+import './styles/fonts/HKGroteskWide-Light.woff'
+import './styles/fonts/HKGroteskWide-Light.woff2'
 import './index.css'
 
 

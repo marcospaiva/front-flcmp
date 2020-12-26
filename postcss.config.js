df69a0f400f903c1e9900138a0682430700284ab
Marcos <marcos@nuvemshop.com.br>
2020-12-26 09:29:33 -0300

    Improve post-css plugins

--- postcss.config.js
+++ postcss.config.js
@@ -7,6 +7,7 @@ module.exports = {
         require('postcss-nested'),
         require('postcss-for'),
         require('postcss-random'),
+        require('postcss-jsmath'),
         require('autoprefixer'),
     ],
 };
